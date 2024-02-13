0c682b1c4a84453be4c3603efc2de53eb788c836
Ilya Stolyar <ilya22@inbox.ru>
2024-02-13 20:40:33 +0300

    add category edit function

--- frontend/src/components/income.js
+++ frontend/src/components/income.js
@@ -1,23 +1,22 @@
 import {CustomHttp} from "../services/custom-http.js";
 import config from "../../config/config.js";
+import {UrlManager} from "../utils/url-manager.js";
 
 export class Income {
     constructor() {
-        this.editButtons = document.querySelectorAll('.income .categories .category .category-buttons .edit');
-        this.editButtons.forEach(editButton => {
-            editButton.addEventListener('click', () => {
-                location.href = '#/income/edit';
-            })
-        });
-
+        this.routeParams = UrlManager.getQueryParams();
         this.addButton = document.getElementById('add');
         if (this.addButton) {
             this.addButton.addEventListener('click', () => {
                 location.href = '#/income/create';
             });
         }
+        if (location.hash === '#/income') {
+            this.getCategories();
+        }
 
-        this.getCategories();
+        this.editCategory();
+
     }
 
     async getCategories() {
@@ -27,20 +26,21 @@ export class Income {
                 if (result.error) {
                     throw new Error(result.error)
                 }
-                console.log(result);
                 let categoriesGridElement = document.querySelector('.categories-grid');
 
-                result.forEach(item=> {
+                for (let i = result.length - 1; i >= 0; i--) {
                     let colElement = document.createElement('div');
                     colElement.classList.add('col');
+                    colElement.setAttribute('id', result[i].id);
                     let categoryElement = document.createElement('div');
                     categoryElement.classList.add('category');
                     let categoryTitleElement = document.createElement('div');
                     categoryTitleElement.classList.add('category-title', 'content-title');
                     let categoryButtonsElement = document.createElement('div');
                     categoryButtonsElement.classList.add('category-buttons');
-                    let editButtonElement = document.createElement('button');
+                    let editButtonElement = document.createElement('a');
                     editButtonElement.classList.add('edit', 'category-button', 'btn', 'btn-primary', 'my-btn');
+                    editButtonElement.setAttribute('data-id', result[i].id);
                     editButtonElement.innerText = 'Редактировать';
                     let deleteButtonElement = document.createElement('button');
                     deleteButtonElement.classList.add('delete', 'category-button', 'btn', 'btn-danger', 'my-btn');
@@ -49,15 +49,50 @@ export class Income {
                     deleteButtonElement.setAttribute('data-bs-target', '#deleteModal');
                     categoryButtonsElement.appendChild(editButtonElement);
                     categoryButtonsElement.appendChild(deleteButtonElement);
-                    categoryTitleElement.innerText = item.title;
+                    categoryTitleElement.innerText = result[i].title;
                     categoryElement.appendChild(categoryTitleElement);
                     categoryElement.appendChild(categoryButtonsElement);
                     colElement.appendChild(categoryElement);
                     categoriesGridElement.insertAdjacentElement('afterbegin', colElement);
+                }
+
+                document.querySelectorAll('.income .categories .category-buttons .edit').forEach(buttonEdit => {
+                    let buttonEditId = buttonEdit.getAttribute('data-id');
+                    buttonEdit.addEventListener('click', () => {
+                        location.href = '#/income/edit?id=' + buttonEditId;
+                    });
                 });
+
             }
         } catch (error) {
             console.log(error);
         }
-    }
+    };
+
+    async editCategory() {
+        let saveBtn = document.getElementById('save-btn');
+        let incomeEditInput = document.getElementById('income-edit-input');
+        if (this.routeParams.id) {
+            try {
+                const result = await CustomHttp.request(config.host + '/categories/income/' + this.routeParams.id);
+                if (result) {
+                    if (result.error) {
+                        throw new Error(result.error);
+                    }
+                    incomeEditInput.value = result.title;
+                }
+
+                if (incomeEditInput.value) {
+                    saveBtn.addEventListener('click',()=> {
+                       CustomHttp.request(config.host + '/categories/income/' + this.routeParams.id, 'PUT', {
+                           title: incomeEditInput.value
+                       })
+                        location.hash = '#/income';
+                    });
+                }
+            } catch (error) {
+
+            }
+        }
+    };
 }
